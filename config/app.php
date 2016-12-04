0383a98db9892cd82bb30cb14c73afec7042e0a4
Barry van Veen <barryvanveen@gmail.com>
2016-12-04 17:47:49 +0100

    removed barryvanveen/laravel-lastfm

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvanveen\Lastfm\LastfmServiceProvider::class,
         JeroenG\Packager\PackagerServiceProvider::class,
 
     ],
